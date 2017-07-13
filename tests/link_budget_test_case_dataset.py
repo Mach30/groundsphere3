0da2edd89fa8afc893ddab0896fd6b98cbb8cc65
CSS <23420656+m30-css@users.noreply.github.com>
2017-07-13 00:31:59 +0000

    Added output testing to test cases

--- tests/link_budget_test_case_dataset.py
+++ tests/link_budget_test_case_dataset.py
@@ -11,11 +11,13 @@ class LinkBudgetTestCaseDataset():
     
         self._datatable = []
 
+        # first test case
         data = self._Data()
         data.name = 'NOAA Weather Satellite'
         data.description = 'A standard NOAA weather satellite, this is a dummy long description ' \
             'that will later be updated to be an accurate description of the test case, instead ' \
             'of this long winded thing that is clearly not meant to be part of real documentation.'
+        # inputs
         data.downlink_frequency = 137.5 * units.megahertz
         data.target_energy_noise_ratio = 20.0 # dB
         data.implementation_loss = 1.0 # dB
@@ -30,6 +32,12 @@ class LinkBudgetTestCaseDataset():
         data.receiving_pointing_loss = -3.0 # dB 
         data.system_noise_figure = 5.0 # dB
         data.noise_bandwidth = 34.0 * units.kilohertz
+        # outputs
+        data.received_power = -101.4 # dB
+        data.minimum_detectable_signal = -123.7 # dB
+        data.energy_noise_ratio = 22.3 # dB
+        data.link_margin = 1.3 # dB
+        # add it to the data table
         self._datatable.append(data)
 
     def __getitem__(self, key):
