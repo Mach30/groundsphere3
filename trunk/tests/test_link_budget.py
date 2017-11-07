a9859b28792adbc7d9e1e84426730547ee00291a
skylarh <skylarh@277c4c60-165c-11e6-b0c5-dd31679fb568>
2017-11-07 05:26:24 +0000

    Updated test code to run the calculation function "run"

--- trunk/tests/test_link_budget.py
+++ trunk/tests/test_link_budget.py
@@ -43,6 +43,7 @@ class TestLinkBudget(unittest.TestCase):
 		self.assertIsNotNone(tc_data.downlink_frequency)
 		self.assertIsNotNone(tc_data.target_energy_noise_ratio)
 		self.assertIsNotNone(tc_data.implementation_loss)
+		self.assertIsNotNone(tc_data.transmit_power)
 		self.assertIsNotNone(tc_data.transmit_losses)
 		self.assertIsNotNone(tc_data.transmit_antenna_gain)
 		self.assertIsNotNone(tc_data.transmit_pointing_loss)
@@ -69,17 +70,61 @@ class TestLinkBudget(unittest.TestCase):
 		self.assertTrue(hasattr(tc_data, 'minimum_detectable_signal'))
 		self.assertTrue(hasattr(tc_data, 'energy_noise_ratio'))
 		self.assertTrue(hasattr(tc_data, 'link_margin'))
-
-		# TODO: tests that validate that the calculations are successfully performed, and match the
-		# expected output.
 		
+		# create the calculator
 		lb_calc = LinkBudgetCalculator(self.ureg)
 		
-		# TODO: load the data
+		# load the data
+		lb_calc.altitude_ground_station   = tc_data.altitude_ground_station
+		lb_calc.altitude_satellite        = tc_data.altitude_satellite
+		lb_calc.orbit_elevation_angle     = tc_data.orbit_elevation_angle
+		lb_calc.downlink_frequency        = tc_data.downlink_frequency
+		lb_calc.target_energy_noise_ratio = tc_data.target_energy_noise_ratio
+		lb_calc.implementation_loss       = tc_data.implementation_loss
+		lb_calc.transmit_power            = tc_data.transmit_power
+		lb_calc.transmit_losses           = tc_data.transmit_losses
+		lb_calc.transmit_antenna_gain     = tc_data.transmit_antenna_gain
+		lb_calc.transmit_pointing_loss    = tc_data.transmit_pointing_loss
+		lb_calc.polarization_losses       = tc_data.polarization_losses
+		lb_calc.atmospheric_loss          = tc_data.atmospheric_loss
+		lb_calc.receiver_gain             = tc_data.receiver_gain
+		lb_calc.system_noise_figure       = tc_data.system_noise_figure
+		lb_calc.noise_bandwidth           = tc_data.noise_bandwidth
 		
-		lb_calc.altitude_ground_station = tc_data.altitude_ground_station
-		
+		# test for equality
 		self.assertEqual(lb_calc.altitude_ground_station, tc_data.altitude_ground_station)
+		self.assertEqual(lb_calc.altitude_satellite, tc_data.altitude_satellite)
+		self.assertEqual(lb_calc.orbit_elevation_angle, tc_data.orbit_elevation_angle)
+		self.assertEqual(lb_calc.downlink_frequency, tc_data.downlink_frequency)
+		self.assertEqual(lb_calc.target_energy_noise_ratio, tc_data.target_energy_noise_ratio)
+		self.assertEqual(lb_calc.implementation_loss, tc_data.implementation_loss)
+		self.assertEqual(lb_calc.transmit_power, tc_data.transmit_power)
+		self.assertEqual(lb_calc.transmit_losses, tc_data.transmit_losses)
+		self.assertEqual(lb_calc.transmit_antenna_gain, tc_data.transmit_antenna_gain)
+		self.assertEqual(lb_calc.transmit_pointing_loss, tc_data.transmit_pointing_loss)
+		self.assertEqual(lb_calc.polarization_losses, tc_data.polarization_losses)
+		self.assertEqual(lb_calc.atmospheric_loss, tc_data.atmospheric_loss)
+		self.assertEqual(lb_calc.receiver_gain, tc_data.receiver_gain)
+		self.assertEqual(lb_calc.system_noise_figure, tc_data.system_noise_figure)
+		self.assertEqual(lb_calc.noise_bandwidth, tc_data.noise_bandwidth)
+		
+		# run the calculations
+		lb_calc.run()
+		
+		# test that the output is valid
+		self.assertEqual(lb_calc.is_valid, True)
+		
+		# test for equality
+		self.assertTrue(abs(lb_calc.downlink_wavelength - tc_data.downlink_wavelength) < 0.001 * self.ureg.meter)
+		self.assertTrue(abs(lb_calc.link_distance - tc_data.link_distance) < 100 * self.ureg.kilometer)
+		self.assertTrue(abs(lb_calc.required_ebno - tc_data.required_ebno) < 0.1)
+		self.assertTrue(abs(lb_calc.transmit_power_dBm - tc_data.transmit_power_dBm) < 0.1)
+		self.assertTrue(abs(lb_calc.transmit_eirp - tc_data.transmit_eirp) < 0.1)
+		self.assertTrue(abs(lb_calc.downlink_path_loss - tc_data.downlink_path_loss) < 10)
+		self.assertTrue(abs(lb_calc.received_power - tc_data.received_power) < 10)
+		self.assertTrue(abs(lb_calc.minimum_detectable_signal - tc_data.minimum_detectable_signal) < 1)
+		self.assertTrue(abs(lb_calc.energy_noise_ratio - tc_data.energy_noise_ratio) < 1)
+		self.assertTrue(abs(lb_calc.link_margin - tc_data.link_margin) < 0.1)
 
 if __name__ == '__main__':
 	unittest.main()
