46af97e932def4a3e393a5c5d882e8bb070138b2
skylarh <skylarh@277c4c60-165c-11e6-b0c5-dd31679fb568>
2017-12-05 01:15:34 +0000

    Added first set of test cases and updated calculations to account for divide by zero elevation angle case

--- trunk/tests/link_budget_test_case_dataset.py
+++ trunk/tests/link_budget_test_case_dataset.py
@@ -18,7 +18,9 @@ class LinkBudgetTestCaseDataset():
 
         self._datatable = []
 
-        # first test case
+		# ---------------------------------------------------------------------------------------
+        # ------------------------------ first test case ----------------------------------------
+		# ---------------------------------------------------------------------------------------
         data = self._Data()
         data.name = 'NOAA Weather Satellite'
         data.description = 'Standard link budget for the groundsphere Mk 3 setup. Includes      ' \
@@ -54,7 +56,259 @@ class LinkBudgetTestCaseDataset():
         data.received_power = -100                          # dBm
         data.minimum_detectable_signal = -124               # dBm
         data.energy_noise_ratio = 23                        # dB
-        data.link_margin = 2                                # dB
+        data.link_margin = 1.7                              # dB
+        # add it to the data table
+        self._datatable.append(data)
+		
+		# ---------------------------------------------------------------------------------------
+        # ------------------------------ second test case ---------------------------------------
+		# ---------------------------------------------------------------------------------------
+        data = self._Data()
+        data.name = 'Ground Sphere Mk. 3 High Elevation Angle'
+        data.description = 'Modified link budget for the groundsphere Mk 3 setup with high elevation angle. Includes      ' \
+            'values for eggbeater antenna, NOAA 19 weather satellite, inclined orbit, and ' \
+            '137.5 MHz APT data. Used as target link budget calculation.' \
+            'Developed by Mach 30 team using known, researched system values. '
+        data.reference = 'https://www.wmo-sat.info/oscar/satellites/view/341'
+        # inputs
+        data.altitude_ground_station = 400 * ureg.meter     # m
+        data.altitude_satellite = 860 * ureg.kilometer      # km
+        data.orbit_elevation_angle = 90 * ureg.degree       # deg
+        data.downlink_frequency = 137.5 * ureg.megahertz    # Hz
+        data.target_energy_noise_ratio = 20.0               # dB
+        data.implementation_loss = -1.0                     # dB
+        data.transmit_power = 5.0 * ureg.watt               # Watt
+        data.transmit_losses = -1.0                         # dB 
+        data.transmit_antenna_gain = 4.0                    # dB
+        data.transmit_pointing_loss = -3.0                  # dB
+        data.polarization_losses = 0.0                      # dB
+        data.atmospheric_loss = 0.0                         # dB
+        data.receive_antenna_gain = 5.4                     # dB
+        data.receiving_pointing_loss = 0.0                  # dB 
+        data.system_noise_figure = 5.0                      # dB
+        data.noise_bandwidth = 34.0 * ureg.kilohertz        # Hz
+        # intermediates
+        data.downlink_wavelength = 2.180 * ureg.meter       # m
+        data.link_distance = 860 * ureg.kilometer           # km
+        data.required_ebno = 21.0                           # dB
+        data.transmit_power_dBm = 37.0                      # dBm
+        data.transmit_eirp = 37.0                           # dBm
+        data.downlink_path_loss = -130                      # dB
+        # outputs
+        data.received_power = -100                          # dBm
+        data.minimum_detectable_signal = -124               # dBm
+        data.energy_noise_ratio = 32                        # dB
+        data.link_margin = 11.2                             # dB
+        # add it to the data table
+        self._datatable.append(data)
+		
+		# ---------------------------------------------------------------------------------------
+        # ------------------------------ third test case ----------------------------------------
+		# ---------------------------------------------------------------------------------------
+        data = self._Data()
+        data.name = 'Ground Sphere Mk. 3 Low Elevation Angle'
+        data.description = 'Modified link budget for the groundsphere Mk 3 setup with low elevation angle. Includes      ' \
+            'values for eggbeater antenna, NOAA 19 weather satellite, inclined orbit, and ' \
+            '137.5 MHz APT data. Used as target link budget calculation.' \
+            'Developed by Mach 30 team using known, researched system values. '
+        data.reference = 'https://www.wmo-sat.info/oscar/satellites/view/341'
+        # inputs
+        data.altitude_ground_station = 400 * ureg.meter     # m
+        data.altitude_satellite = 860 * ureg.kilometer      # km
+        data.orbit_elevation_angle =  5 * ureg.degree       # deg
+        data.downlink_frequency = 137.5 * ureg.megahertz    # Hz
+        data.target_energy_noise_ratio = 20.0               # dB
+        data.implementation_loss = -1.0                     # dB
+        data.transmit_power = 5.0 * ureg.watt               # Watt
+        data.transmit_losses = -1.0                         # dB 
+        data.transmit_antenna_gain = 4.0                    # dB
+        data.transmit_pointing_loss = -6.0                  # dB
+        data.polarization_losses = 0.0                      # dB
+        data.atmospheric_loss = -2.1                        # dB
+        data.receive_antenna_gain = 5.4                     # dB
+        data.receiving_pointing_loss = -3.0                 # dB 
+        data.system_noise_figure = 5.0                      # dB
+        data.noise_bandwidth = 34.0 * ureg.kilohertz        # Hz
+        # intermediates
+        data.downlink_wavelength = 2.180 * ureg.meter       # m
+        data.link_distance = 2900 * ureg.kilometer          # km
+        data.required_ebno = 21.0                           # dB
+        data.transmit_power_dBm = 37.0                      # dBm
+        data.transmit_eirp = 34.0                           # dBm
+        data.downlink_path_loss = -140                      # dB
+        # outputs
+        data.received_power = -110                          # dBm
+        data.minimum_detectable_signal = -124               # dBm
+        data.energy_noise_ratio = 13                        # dB
+        data.link_margin = -7.5                             # dB
+        # add it to the data table
+        self._datatable.append(data)
+		
+		# ---------------------------------------------------------------------------------------
+        # ------------------------------ fourth test case ---------------------------------------
+		# ---------------------------------------------------------------------------------------
+        data = self._Data()
+        data.name = 'Ground Sphere Mk. 3 High Elevation Angle'
+        data.description = 'Modified link budget for the groundsphere Mk 3 setup with high elevation angle. Includes      ' \
+            'values for eggbeater antenna, NOAA 19 weather satellite, inclined orbit, and ' \
+            '137.5 MHz APT data. Used as target link budget calculation.' \
+            'Developed by Mach 30 team using known, researched system values. '
+        data.reference = 'https://www.wmo-sat.info/oscar/satellites/view/341'
+        # inputs
+        data.altitude_ground_station = 400 * ureg.meter     # m
+        data.altitude_satellite = 860 * ureg.kilometer      # km
+        data.orbit_elevation_angle = 45 * ureg.degree       # deg
+        data.downlink_frequency = 137.5 * ureg.megahertz    # Hz
+        data.target_energy_noise_ratio = 20.0               # dB
+        data.implementation_loss = -1.0                     # dB
+        data.transmit_power = 5.0 * ureg.watt               # Watt
+        data.transmit_losses = -1.0                         # dB 
+        data.transmit_antenna_gain = 4.0                    # dB
+        data.transmit_pointing_loss = -3.0                  # dB
+        data.polarization_losses = 0.0                      # dB
+        data.atmospheric_loss = -0.3                        # dB
+        data.receive_antenna_gain = 24                      # dB
+        data.receiving_pointing_loss = 0.0                 # dB 
+        data.system_noise_figure = 5.0                      # dB
+        data.noise_bandwidth = 34.0 * ureg.kilohertz        # Hz
+        # intermediates
+        data.downlink_wavelength = 2.180 * ureg.meter       # m
+        data.link_distance = 1200 * ureg.kilometer          # km
+        data.required_ebno = 21.0                           # dB
+        data.transmit_power_dBm = 37.0                      # dBm
+        data.transmit_eirp = 37.0                           # dBm
+        data.downlink_path_loss = -140                      # dB
+        # outputs
+        data.received_power = -80                           # dBm
+        data.minimum_detectable_signal = -124               # dBm
+        data.energy_noise_ratio = 48                        # dB
+        data.link_margin = 26.9                             # dB
+        # add it to the data table
+        self._datatable.append(data)
+		
+		# ---------------------------------------------------------------------------------------
+        # ------------------------------ fifth test case ---------------------------------------
+		# ---------------------------------------------------------------------------------------
+        data = self._Data()
+        data.name = 'Ground Sphere Mk. 3 High Elevation Angle'
+        data.description = 'Modified link budget for the groundsphere Mk 3 setup with high elevation angle. Includes      ' \
+            'values for eggbeater antenna, NOAA 19 weather satellite, inclined orbit, and ' \
+            '137.5 MHz APT data. Used as target link budget calculation.' \
+            'Developed by Mach 30 team using known, researched system values. '
+        data.reference = 'https://www.wmo-sat.info/oscar/satellites/view/341'
+        # inputs
+        data.altitude_ground_station = 400 * ureg.meter     # m
+        data.altitude_satellite = 860 * ureg.kilometer      # km
+        data.orbit_elevation_angle = 45 * ureg.degree       # deg
+        data.downlink_frequency = 137.5 * ureg.megahertz    # Hz
+        data.target_energy_noise_ratio = 20.0               # dB
+        data.implementation_loss = -1.0                     # dB
+        data.transmit_power = 5.0 * ureg.watt               # Watt
+        data.transmit_losses = -1.0                         # dB 
+        data.transmit_antenna_gain = 4.0                    # dB
+        data.transmit_pointing_loss = -3.0                  # dB
+        data.polarization_losses = 0.0                      # dB
+        data.atmospheric_loss = -0.3                        # dB
+        data.receive_antenna_gain = 24                      # dB
+        data.receiving_pointing_loss = -30                  # dB 
+        data.system_noise_figure = 5.0                      # dB
+        data.noise_bandwidth = 34.0 * ureg.kilohertz        # Hz
+        # intermediates
+        data.downlink_wavelength = 2.180 * ureg.meter       # m
+        data.link_distance = 1200 * ureg.kilometer          # km
+        data.required_ebno = 21.0                           # dB
+        data.transmit_power_dBm = 37.0                      # dBm
+        data.transmit_eirp = 37.0                           # dBm
+        data.downlink_path_loss = -140                      # dB
+        # outputs
+        data.received_power = -110                          # dBm
+        data.minimum_detectable_signal = -124               # dBm
+        data.energy_noise_ratio = 18                        # dB
+        data.link_margin = -3                               # dB
+        # add it to the data table
+        self._datatable.append(data)
+		
+		# ---------------------------------------------------------------------------------------
+        # ------------------------------ sixth test case ---------------------------------------
+		# ---------------------------------------------------------------------------------------
+        data = self._Data()
+        data.name = 'Ground Sphere Mk. 3 High Elevation Angle'
+        data.description = 'Modified link budget for the groundsphere Mk 3 setup with high elevation angle. Includes      ' \
+            'values for eggbeater antenna, NOAA 19 weather satellite, inclined orbit, and ' \
+            '137.5 MHz APT data. Used as target link budget calculation.' \
+            'Developed by Mach 30 team using known, researched system values. '
+        data.reference = 'https://www.wmo-sat.info/oscar/satellites/view/341'
+        # inputs
+        data.altitude_ground_station = 400 * ureg.meter     # m
+        data.altitude_satellite = 860 * ureg.kilometer      # km
+        data.orbit_elevation_angle = 25 * ureg.degree       # deg
+        data.downlink_frequency = 137.5 * ureg.megahertz    # Hz
+        data.target_energy_noise_ratio = 20.0               # dB
+        data.implementation_loss = -1.0                     # dB
+        data.transmit_power = 5.0 * ureg.watt               # Watt
+        data.transmit_losses = -1.0                         # dB 
+        data.transmit_antenna_gain = 4.0                    # dB
+        data.transmit_pointing_loss = -3.0                  # dB
+        data.polarization_losses = -3.0                     # dB
+        data.atmospheric_loss = -0.75                       # dB
+        data.receive_antenna_gain = 2.1                     # dB
+        data.receiving_pointing_loss = 0.0                  # dB 
+        data.system_noise_figure = 5.0                      # dB
+        data.noise_bandwidth = 34.0 * ureg.kilohertz        # Hz
+        # intermediates
+        data.downlink_wavelength = 2.180 * ureg.meter       # m
+        data.link_distance = 1700 * ureg.kilometer          # km
+        data.required_ebno = 21.0                           # dB
+        data.transmit_power_dBm = 37.0                      # dBm
+        data.transmit_eirp = 37.0                           # dBm
+        data.downlink_path_loss = -140                      # dB
+        # outputs
+        data.received_power = -100                          # dBm
+        data.minimum_detectable_signal = -124               # dBm
+        data.energy_noise_ratio = 19                        # dB
+        data.link_margin = -1.6                             # dB
+        # add it to the data table
+        self._datatable.append(data)
+		
+		# ---------------------------------------------------------------------------------------
+        # ------------------------------ seventh test case --------------------------------------
+		# ---------------------------------------------------------------------------------------
+        data = self._Data()
+        data.name = 'Ground Sphere Mk. 3 High Elevation Angle'
+        data.description = 'Modified link budget for the groundsphere Mk 3 setup with high elevation angle. Includes      ' \
+            'values for eggbeater antenna, NOAA 19 weather satellite, inclined orbit, and ' \
+            '137.5 MHz APT data. Used as target link budget calculation.' \
+            'Developed by Mach 30 team using known, researched system values. '
+        data.reference = 'https://www.wmo-sat.info/oscar/satellites/view/341'
+        # inputs
+        data.altitude_ground_station = 400 * ureg.meter     # m
+        data.altitude_satellite = 860 * ureg.kilometer      # km
+        data.orbit_elevation_angle = 90 * ureg.degree       # deg
+        data.downlink_frequency = 137.5 * ureg.megahertz    # Hz
+        data.target_energy_noise_ratio = 20.0               # dB
+        data.implementation_loss = -1.0                     # dB
+        data.transmit_power = 5.0 * ureg.watt               # Watt
+        data.transmit_losses = -1.0                         # dB 
+        data.transmit_antenna_gain = 4.0                    # dB
+        data.transmit_pointing_loss = -3.0                  # dB
+        data.polarization_losses = -3.0                     # dB
+        data.atmospheric_loss = 0.0                         # dB
+        data.receive_antenna_gain = 2.1                     # dB
+        data.receiving_pointing_loss = -12.0                # dB 
+        data.system_noise_figure = 5.0                      # dB
+        data.noise_bandwidth = 34.0 * ureg.kilohertz        # Hz
+        # intermediates
+        data.downlink_wavelength = 2.180 * ureg.meter       # m
+        data.link_distance = 860 * ureg.kilometer           # km
+        data.required_ebno = 21.0                           # dB
+        data.transmit_power_dBm = 37.0                      # dBm
+        data.transmit_eirp = 37.0                           # dBm
+        data.downlink_path_loss = -140                      # dB
+        # outputs
+        data.received_power = -106                          # dBm
+        data.minimum_detectable_signal = -124               # dBm
+        data.energy_noise_ratio = 14                        # dB
+        data.link_margin = -7.1                             # dB
         # add it to the data table
         self._datatable.append(data)
 
